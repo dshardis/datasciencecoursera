6aaa54a953f2c7ce24b7bac7e467000b3dc45f04
dshardis <dshardis@uncc.edu>
2014-12-07 18:59:01 -0500

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,8 @@
+data <- data.frame()
+data <- read.csv("household_power_consumption.txt",sep = ";", na.strings = "?")
+dataset <- subset(data,data$Date == "1/2/2007" | data$Date == "2/2/2007")
+dataset <- na.omit(dataset)
+dataset$timeall <- as.POSIXct(paste(dataset$Date,dataset$Time), format = "%d/%m/%Y %H:%M")
+png(filename = "plot1.png", width = 480, height = 480)
+hist(dataset$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
